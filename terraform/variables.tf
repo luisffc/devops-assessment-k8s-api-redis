873cb6ed5262bbb5f133537caa6f54f17eb024c0
Luis Cavalcante <luis@coffeetech.com.br>
2025-06-27 20:08:33 -0300

    adding terraform
    Signed-off-by: Luis Cavalcante <luis@coffeetech.com.br>

---
+++ terraform/variables.tf
@@ -0,0 +1,103 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "devops-assessment"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether the EKS cluster API server endpoint should be accessible from public internet"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_endpoint_private_access" {
+  description = "Indicates whether the EKS cluster API server endpoint should be accessible from within the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "node_group_instance_types" {
+  description = "EC2 instance types for the EKS node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "node_group_scaling_config" {
+  description = "Scaling configuration for the EKS node group"
+  type = object({
+    desired_size = number
+    max_size     = number
+    min_size     = number
+  })
+  default = {
+    desired_size = 3
+    max_size     = 10
+    min_size     = 1
+  }
+}
+
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+  default     = "your-github-org"
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "devops-assessment-k8s-api-redis"
+}
+
+variable "enable_irsa" {
+  description = "Whether to enable IAM Roles for Service Accounts"
+  type        = bool
+  default     = true
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap"
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap"
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Project     = "devops-assessment"
+    Environment = "dev"
+  }
+}
