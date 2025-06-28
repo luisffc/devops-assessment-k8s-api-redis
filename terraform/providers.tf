fad483e0da57ad214721a256c171170b041ec340
Luis Cavalcante <luis@coffeetech.com.br>
2025-06-27 22:27:30 -0300

    providers fixes
    Signed-off-by: Luis Cavalcante <luis@coffeetech.com.br>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -20,26 +20,26 @@ data "aws_availability_zones" "available" {
 
 # Configure Kubernetes provider
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = try(module.eks.cluster_endpoint, "")
+  cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    args        = ["eks", "get-token", "--cluster-name", local.name]
   }
 }
 
 # Configure Helm provider
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = try(module.eks.cluster_endpoint, "")
+    cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      args        = ["eks", "get-token", "--cluster-name", local.name]
     }
   }
 }
